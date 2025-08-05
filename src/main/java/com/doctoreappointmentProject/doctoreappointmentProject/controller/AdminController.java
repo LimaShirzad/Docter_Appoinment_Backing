7fa25a76a9342547528e2385ebd2c84cfd16030d
LimaSherzad <limasherzad55@gmail.com>
2025-08-05 08:56:34 +0430

    Arange the folder strtucture

--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/AdminController.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/AdminController.java
@@ -1,11 +1,10 @@
-package com.doctoreappointmentProject.doctoreappointmentProject.Admin.Controller;
+package com.doctoreappointmentProject.doctoreappointmentProject.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/websiteController.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/websiteController.java
@@ -1,4 +1,4 @@
-package com.doctoreappointmentProject.doctoreappointmentProject.AllwebsitePart;
+package com.doctoreappointmentProject.doctoreappointmentProject.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
