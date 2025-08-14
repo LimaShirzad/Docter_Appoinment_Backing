41ab1889133331cfc3dccafa3288590239918d52
LimaSherzad <limasherzad55@gmail.com>
2025-08-14 11:58:05 +0430

    remove validation

--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/util/ValidationUtil.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/util/ValidationUtil.java
@@ -39,20 +39,7 @@ public class ValidationUtil {
         return value !=null && ! value.trim().isEmpty();
 
    }
-   public  static String cleanString(String value){
-        if(value==null)
-            return  null;
 
-        return  value.trim().replaceAll("\\s+"," ");
-
-   }
-
-    public static String capitalizeFirstLetter(String input) {
-        if (input == null || input.isEmpty()) {
-            return input;
-        }
-        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
-    }
 
 
 
