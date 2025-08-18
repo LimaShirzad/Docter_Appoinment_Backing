71395b2884285fa4bc19f5643d7d72e8700a37d3
LimaSherzad <limasherzad55@gmail.com>
2025-08-18 10:06:17 +0430

    solve depancy bug

--- src/test/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/RoleControllerTest.java
+++ src/test/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/RoleControllerTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,7 +22,7 @@ import org.springframework.http.MediaType;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static reactor.core.publisher.Mono.when;
+
 
 
 @WebMvcTest(RoleController.class)
