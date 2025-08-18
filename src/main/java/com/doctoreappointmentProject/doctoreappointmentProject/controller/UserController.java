e602426dcd893f21f4f254302bb77b0c5d211638
LimaSherzad <limasherzad55@gmail.com>
2025-08-18 19:28:32 +0430

    remove user opertion

--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/UserController.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/UserController.java
@@ -1,33 +1,4 @@
 package com.doctoreappointmentProject.doctoreappointmentProject.controller;
 
-
-import com.doctoreappointmentProject.doctoreappointmentProject.model.User;
-import com.doctoreappointmentProject.doctoreappointmentProject.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/user")
 public class UserController {
-
-    @Autowired
-
-    private UserService userService;
-
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-
-    @PostMapping
-    public User createUser(@Valid @RequestBody User user)
-    {
-         return userService.saveUser(user);
-
-    }
-
-
-
 }
