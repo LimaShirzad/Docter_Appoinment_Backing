faf09f06d96fd0e8fc076db12c23c7b1e996485e
LimaSherzad <limasherzad55@gmail.com>
2025-08-19 12:12:43 +0430

    add delete get all user

--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/UserController.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/controller/UserController.java
@@ -1,32 +1,98 @@
 package com.doctoreappointmentProject.doctoreappointmentProject.controller;
 
 
+import com.doctoreappointmentProject.doctoreappointmentProject.model.Roles;
 import com.doctoreappointmentProject.doctoreappointmentProject.model.User;
+import com.doctoreappointmentProject.doctoreappointmentProject.service.RoleService;
 import com.doctoreappointmentProject.doctoreappointmentProject.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
+//@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
-
     private UserService userService;
+    @Autowired
+    private RoleService roleService;
+
+    private final Map<String,String> userResponse=new HashMap<>();
 
 
     public UserController(UserService userService) {
+
         this.userService = userService;
     }
 
 
     @PostMapping
-    public User createUser(@Valid @RequestBody User user)
-    {
-         return userService.saveUser(user);
+    public ResponseEntity<Map<String,String>> createUser(@Valid @RequestBody User user) {
+
+        User saveUser=userService.saveUser(user);
+
+        userResponse.put("userSaveMessage","User Save Successfully");
+
+
+        return new ResponseEntity<>(userResponse,HttpStatus.CREATED);
 
     }
+
+
+    @GetMapping("/roles")
+    public List<Roles> getAllRoles(){
+
+        return  roleService.getAllRoles();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<Map<String,String>> deleteUserById(@PathVariable Long id){
+
+         userService.deleteUser(id);
+
+        userResponse.put("userDeleteMessage","User Deleted Successfully");
+
+        return ResponseEntity.ok(userResponse);
+
+
+    }
+
+    @GetMapping("/{all_User}")
+    public  ResponseEntity<Object> getAllUser(){
+
+          List<User> users=userService.getAllUser();
+
+          if(users.isEmpty()){
+                  userResponse.put("message","No User Found");
+          }
+          return ResponseEntity.ok(users);
+
+    }
+
+    @GetMapping("/get/{id}")
+    public User getUserId(@PathVariable Long id){
+
+        return userService.getUserById(id);
+
+    }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/service/UserService.java
+++ src/main/java/com/doctoreappointmentProject/doctoreappointmentProject/service/UserService.java
@@ -4,6 +4,8 @@ import com.doctoreappointmentProject.doctoreappointmentProject.repository.UserRe
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,8 +19,33 @@ public class UserService {
            return userRepository.save(user);
     }
 
+    public  void deleteUser(Long id){
+
+       if(!userRepository.existsById(id)) {
+
+           throw new RuntimeException("User Not Found Withe ID  " + id);
+       }
+
+        userRepository.deleteById(id);
+
+    }
 
 
+    public List<User> getAllUser(){
+
+        return userRepository.findAll();
+
+    }
+
+
+    public User getUserById(Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found With ID " + id));
+
+
+
+    }
 }
 
 
